ee06f9814db0588f5800fa9b8e59f9d6d133e5b3
rshaikh2200 <rizwanshaikh2200@gmail.com>
2024-08-17 00:34:48 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { auth } from '../app/firebase'; 
 import AuthPage from '../app/components/AuthPage'; 
 import Home from '../app/components/page'; 
 import { useAuthState } from '../app/hooks/useAuthState';
 
+const defaultTheme = createTheme(); // Create a default theme
+
 export default function Index() {
   const { user } = useAuthState();
 
-  return user ? <Home /> : <AuthPage />;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      {user ? <Home /> : <AuthPage />}
+    </ThemeProvider>
+  );
 }
