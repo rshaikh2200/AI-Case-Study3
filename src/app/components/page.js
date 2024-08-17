db8b840f58c0dcabf217f9caae199182e89ff773
rshaikh2200 <rizwanshaikh2200@gmail.com>
2024-08-17 21:11:31 +0000

    change

--- src/app/components/page.js
+++ src/app/components/page.js
@@ -8,6 +8,7 @@ import LogoutIcon from '@mui/icons-material/Logout';
 import AddIcon from '@mui/icons-material/Add';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
+import React from 'react';
 
 // Dark mode theme
 const darkTheme = createTheme({
