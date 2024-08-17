ec11fa7ddd6ec6c0cf6e3724dc1972103644adf3
rshaikh2200 <rizwanshaikh2200@gmail.com>
2024-08-17 00:32:02 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { auth } from '../app/firebase'; 
+import AuthPage from '../app/components/AuthPage'; 
 import Home from '../app/components/page'; 
 import { useAuthState } from '../app/hooks/useAuthState';
 
 export default function Index() {
   const { user } = useAuthState();
 
-  return user ? <Home /> : null;
+  return user ? <Home /> : <AuthPage />;
 }
