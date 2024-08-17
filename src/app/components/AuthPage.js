1ad676fbe970e345096af38d6e47e07ce4f82a52
rshaikh2200 <rizwanshaikh2200@gmail.com>
2024-08-17 23:26:12 +0000

    cam

--- src/app/components/AuthPage.js
+++ src/app/components/AuthPage.js
@@ -66,7 +66,7 @@ export default function AuthPage() {
         sm={4}
         md={7}
         sx={{
-          backgroundImage: 'url("src/app/images.jpeg")', // Update this path to your background image
+          backgroundImage: 'url("/src/app/images.jpeg")', // Update this path to your background image
           backgroundColor: (t) =>
             t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
           backgroundSize: 'cover',
