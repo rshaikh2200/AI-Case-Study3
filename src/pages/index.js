1000aea41ccb4c27424e61c56c445f24188ccbaa
rshaikh2200 <rizwanshaikh2200@gmail.com>
2024-08-17 00:20:28 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { auth } from '../app/firebase'; 
-import AuthPage from '../app/components/AuthPage'; 
 import Home from '../app/components/page'; 
 import { useAuthState } from '../app/hooks/useAuthState';
-import LandingPage from '../app/components/LandingPage'; // Import the LandingPage component
 
 export default function Index() {
   const { user } = useAuthState();
 
-  return user ? <Home />
+  return user ? <Home /> : null;
 }
