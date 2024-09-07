37abc812d73f919fe62331775facf1ca2cbf4789
rshaikh2200 <rizwanshaikh2200@gmail.com>
2024-09-07 13:18:13 +0000

    hi

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,6 @@
 import React from 'react';
-import { auth } from '../app/firebase'; // Corrected the import path
-import AuthPage from '../app/components/AuthPage'; // Corrected the import path
 import Home from '../app/components/page'; // Ensure correct import path
-import { useAuthState } from '../app/hooks/useAuthState';
 
 export default function Index() {
-  const { user } = useAuthState();
-
-  return user ? <Home /> : <AuthPage />;
+  return <Home />;
 }
