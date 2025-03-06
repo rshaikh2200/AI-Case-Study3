2afac1b6d5fd09756ed9d1796a47cded34d82bf5
rshaikh2200 <rizwanshaikh2200@gmail.com>
2025-03-05 21:51:19 -0500

    Update route.js

--- src/app/api/ai-models/route.js
+++ src/app/api/ai-models/route.js
@@ -254,7 +254,7 @@ for a ${care} ${role} specializing in ${specialization}, and working in the ${de
 ${
   department === "Stroke Center"
     ? `Since the department is a Stroke Center, the 150-word case studies should focus on stroke cases
-       and potential medical errors. When describing medical dosage and blood pressure provide quantity and measurements (E.g. 100 Mg). Ensure the medical case studies focus on unique medical error,  and incorporate a variety of possible errors while maintaining clinical integrity. Keep the case studies short and concise.`
+       and potential medical errors. Make sure to provide measurements (E.g. 100 Mg) when needed. Ensure the medical case studies focus on unique medical error,  and incorporate a variety of possible errors while maintaining clinical integrity. Keep the case studies short and concise and do not mention countries name or where a patient is from. Also do not state team assessment or review of situation.`
     : ""
 }
 
