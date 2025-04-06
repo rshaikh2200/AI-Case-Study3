646548ee02e4cf36bbfa061b17cb76dacf199d37
rshaikh2200 <rizwanshaikh2200@gmail.com>
2025-04-06 01:15:39 -0400

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -14,7 +14,7 @@ export default function Home() {
         headers: {
           'Content-Type': 'application/json',
           // Use your environment variable to securely access the API key
-          'Authorization': `Bearer ${process.env.NEXT_PUBLIC_HF_API_KEY}`,
+          'Authorization': `Bearer ${process.env.HF_API_KEY}`,
         },
         body: JSON.stringify({ inputs: prompt }),
       });
