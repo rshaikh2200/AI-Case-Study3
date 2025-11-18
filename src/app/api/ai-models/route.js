0625c8c231a4878c6d3d24b0d4f3ebb5288ded35
rshaikh2200 <rizwanshaikh2200@gmail.com>
2025-11-18 13:26:52 -0500

    Update route.js

--- src/app/api/ai-models/route.js
+++ src/app/api/ai-models/route.js
@@ -294,13 +294,13 @@ The medical case study should:
     
         **Case Study 3:**
         - True or False Question 1: Focuses on Effective Handoffs
-        - Fill in the Blank Question 2: Focuses on Read and Repeat Back
+        - Fill in the Blank Question 2:Focuses on Read and Repeat Back
         - Multiple Choice Question 3: Focuses on Ask Clarifying Questions
     
         **Case Study 4:**
-        - Question 1: Focuses on Alphanumeric Language
-        - Question 2: Focuses on SBAR
-        - Question 3: Focuses on STAR
+        -  Multiple Choice Question 1: Focuses on Alphanumeric Language
+        -  Multiple Choice Question 2: Focuses on SBAR
+        - Fill in the Blank Question 3: Focuses on STAR
     
     - **Use the following 11 Error Prevention Tools and Definitions:**
     
@@ -446,9 +446,113 @@ The medical case study should:
               "correct answer": "B) True",
               "Hint": "Pause and Refocus."
             }
+             {
+          "caseStudy": "Case Study 2",
+          "scenario": "Mr. Nitesh Patel, a 65 year old patient underwent a total knee replacement surgery for severe osteoarthritis. During the procedure, Brent Keeling a respected orthopedic surgeon noted difficulty in exposing the joint due to significant scarring from the patient's previous knee surgeries. Towards the end of the procedure, the patient complained of numbness and weakness in the foot. Postoperative imaging revealed a stretch injury to the common personeal nerve.",
+          "questions": [
+            {
+              "question": "Whcich EPT practice that involves verifying with a qualified internal source, could have helped Dr. Patel avoid this mix up?",
+              "options": {
+                "A": "Peer Checking and Coaching",
+                "B": "Debrief",
+                "C": "ARCC",
+                "D": "Validate and Verify"
+              },
+              "correct answer": "D) Validate and Verify",
+              "Hint": "Does this make sense to me?, Is it right, based on what I know?, Is this what I expected?, Does this information "fit in with my past experience or other information I may have at this time?"
+            },
+            {
+              "question": "Before documenting an elevated blood pressure and notifying the provider, Dr.Brent should have taken an extra moment to double-check the reading against expected standards. This step is an example of stopping to __ information before acting. ",
+              "options": {
+                "A": "Debrief",
+                "B": "Validate and Verify",
+                "C": "Peer Check",
+                "D": "Ask Clarifying Questions"
+              },
+              "correct answer": "C) ARCC",
+              "Hint": "Ask a question to gently prompt the other person of potential safety issue"
+            },
+            {
+              "question": "If the Orthopedic Surgeon Dr. Brent had paused briefly before taking the patient's blood pressure to focus on proper positioning and whether the patient had used the restroom before, it could have prevented the inaccurate reading.  ",
+              "options": {
+                "A": "True",
+                "B": "False",
+              },
+              "correct answer": "B) True",
+              "Hint": "Pause and Refocus."
+            }
+             {
+          "caseStudy": "Case Study 3",
+          "scenario": "Mr. Nitesh Patel, a 65 year old patient underwent a total knee replacement surgery for severe osteoarthritis. During the procedure, Brent Keeling a respected orthopedic surgeon noted difficulty in exposing the joint due to significant scarring from the patient's previous knee surgeries. Towards the end of the procedure, the patient complained of numbness and weakness in the foot. Postoperative imaging revealed a stretch injury to the common personeal nerve.",
+          "questions": [
+            {
+              "question": "Whcich EPT practice that involves verifying with a qualified internal source, could have helped Dr. Patel avoid this mix up?",
+              "options": {
+                "A": "Peer Checking and Coaching",
+                "B": "Debrief",
+                "C": "ARCC",
+                "D": "Validate and Verify"
+              },
+              "correct answer": "D) Validate and Verify",
+              "Hint": "Does this make sense to me?, Is it right, based on what I know?, Is this what I expected?, Does this information "fit in with my past experience or other information I may have at this time?"
+            },
+            {
+              "question": "Before documenting an elevated blood pressure and notifying the provider, Dr.Brent should have taken an extra moment to double-check the reading against expected standards. This step is an example of stopping to __ information before acting. ",
+              "options": {
+                "A": "Debrief",
+                "B": "Validate and Verify",
+                "C": "Peer Check",
+                "D": "Ask Clarifying Questions"
+              },
+              "correct answer": "C) ARCC",
+              "Hint": "Ask a question to gently prompt the other person of potential safety issue"
+            },
+            {
+              "question": "If the Orthopedic Surgeon Dr. Brent had paused briefly before taking the patient's blood pressure to focus on proper positioning and whether the patient had used the restroom before, it could have prevented the inaccurate reading.  ",
+              "options": {
+                "A": "True",
+                "B": "False",
+              },
+              "correct answer": "B) True",
+              "Hint": "Pause and Refocus."
+            }
+             {
+          "caseStudy": "Case Study 4",
+          "scenario": "Mr. Nitesh Patel, a 65 year old patient underwent a total knee replacement surgery for severe osteoarthritis. During the procedure, Brent Keeling a respected orthopedic surgeon noted difficulty in exposing the joint due to significant scarring from the patient's previous knee surgeries. Towards the end of the procedure, the patient complained of numbness and weakness in the foot. Postoperative imaging revealed a stretch injury to the common personeal nerve.",
+          "questions": [
+            {
+              "question": "Whcich EPT practice that involves verifying with a qualified internal source, could have helped Dr. Patel avoid this mix up?",
+              "options": {
+                "A": "Peer Checking and Coaching",
+                "B": "Debrief",
+                "C": "ARCC",
+                "D": "Validate and Verify"
+              },
+              "correct answer": "D) Validate and Verify",
+              "Hint": "Does this make sense to me?, Is it right, based on what I know?, Is this what I expected?, Does this information "fit in with my past experience or other information I may have at this time?"
+            },
+            {
+              "question": "Before documenting an elevated blood pressure and notifying the provider, Dr.Brent should have taken an extra moment to double-check the reading against expected standards. This step is an example of stopping to __ information before acting. ",
+              "options": {
+                "A": "Debrief",
+                "B": "Validate and Verify",
+                "C": "Peer Check",
+                "D": "Ask Clarifying Questions"
+              },
+              "correct answer": "C) ARCC",
+              "Hint": "Ask a question to gently prompt the other person of potential safety issue"
+            },
+            {
+              "question": "If the Orthopedic Surgeon Dr. Brent had paused briefly before taking the patient's blood pressure to focus on proper positioning and whether the patient had used the restroom before, it could have prevented the inaccurate reading.  ",
+              "options": {
+                "A": "True",
+                "B": "False",
+              },
+              "correct answer": "B) True",
+              "Hint": "Pause and Refocus."
+            }
           ]
         }
-         // Repeat for Case Study 2, 3, and 4
       ]
     }
     \`\`\`
